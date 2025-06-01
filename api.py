591cbe702ecd16e5466753305d4ca15f36b6a95c
Xavier KDG <xavier.kdg@gmail.com>
2025-06-01 15:49:56 +0200

    get_channels testen

--- api.py
+++ api.py
@@ -14,10 +14,25 @@ HEADERS = {
     "Authorization": f"Bearer {API_KEY}",
 }
 
-def get_messages(channel_id):
-    response = requests.get(f"{URL}/channels/{channel_id}/messages", headers=HEADERS)
-    return response.json()
+# def get_messages(channel_id):
+#     response = requests.get(f"{URL}/channels/{channel_id}/messages", headers=HEADERS)
+#     return response.json()
+
+def get_channels():
+    response = requests.get(f"{URL}/channels/", headers=HEADERS)
+    channels = response.json()
+    filter = []
+
+    for i in channels:
+        filter.append({
+            "channel_naam": i.get('name'),
+            "laatst_bijgewerkt": i.get("updated_at"),
+            })
+        
+    return filter  # dit geeft channel_naam en laatst_bijgewerkt, maar laatst_bijgewerkt is in timestamp format
 
 if __name__ == "__main__":
-    messages = get_messages("d968f28b-26e4-40c5-aea9-558c964b01d9")
-    print(messages)
+    # messages = get_messages("d968f28b-26e4-40c5-aea9-558c964b01d9")
+    channels = get_channels()
+    # print(messages)
+    print(channels)
